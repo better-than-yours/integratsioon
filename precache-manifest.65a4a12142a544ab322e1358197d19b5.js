8311c2f02717908c9f0d06baa37d821af5f3d5b6
lafin <lafin@users.noreply.github.com>
2020-08-19 20:16:26 +0000

    Automated deployment: Wed Aug 19 20:16:26 UTC 2020 b25a6a60e100eaba4ab3767f6d613076731c880b

---
+++ precache-manifest.65a4a12142a544ab322e1358197d19b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ce655997843c9340ffca7d7a2b2b0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0b79bfcfcd0e2305b8f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b6bf319624d0505bc764",
+    "url": "/static/js/2.fb3265e5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fb3265e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b79bfcfcd0e2305b8f",
+    "url": "/static/js/main.65e6a6ca.chunk.js"
+  },
+  {
+    "revision": "5df4b8fd012d07e3133d",
+    "url": "/static/js/runtime-main.ff1fcb84.js"
+  }
+]);
